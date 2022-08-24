8b65326ae780d3b82e597d56161e35355cb8c29d
Mikhail Zhilkin <csharper2005@gmail.com>
2022-08-24 16:43:15 +0000

    Add support for Beeline SmartBox Turbo+
    Add support for Beeline SmartBox Turbo+

---
+++ trunk/configs/boards/SBTP/board.h
@@ -0,0 +1,21 @@
+/* Beeline SmartBox Turbo+ */
+
+#define BOARD_PID		"SBTP"
+#define BOARD_NAME		"SBTP"
+#define BOARD_DESC		"Beeline SmartBox Turbo+ Wireless Router"
+#define BOARD_VENDOR_NAME	"Beeline"
+#define BOARD_VENDOR_URL	"https://beeline.ru/"
+#define BOARD_MODEL_URL		"https://beeline.ru/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC		1
+#define BOARD_NUM_ANT_5G_TX		4
+#define BOARD_NUM_ANT_5G_RX		4
+#define BOARD_NUM_ANT_2G_TX		2
+#define BOARD_NUM_ANT_2G_RX		2
+#define BOARD_NUM_ETH_LEDS		0
+#define BOARD_NUM_ETH_EPHY		5
+#define BOARD_HAS_EPHY_L1000		1
+#define BOARD_HAS_EPHY_W1000		1
+#define BOARD_NUM_UPHY_USB3		1
